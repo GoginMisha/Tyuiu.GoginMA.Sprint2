9a2146bb84ba7c4bf4e13ca8eecec4d0408f5449
Tyuiu.GoginMA@outlook.com <partycat53241@gmail.com>
2023-10-15 10:51:42 +0500

    Task3

---
+++ Tyuiu.GoginMA.Sprint2.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.GoginMA.Sprint2.Task3.V10.Lib;
+namespace Tyuiu.GoginMA.Sprint2.Task3.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = 6.333;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = -1;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -1;
+            double res = ds.Calculate(x);
+            double wait = 0.167;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -15;
+            double res = ds.Calculate(x);
+            double wait = -164.933;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}

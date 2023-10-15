0de1fd3d0fb53401561490010ad3b7071cc56f72
Tyuiu.GoginMA@outlook.com <partycat53241@gmail.com>
2023-10-15 16:13:26 +0500

    Task4

---
+++ Tyuiu.GoginMA.Sprint2.Task4.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GoginMA.Sprint2.Task4.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GoginMA.Sprint2.Task4.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ff3dc694-e1e8-400f-aa3c-0214ddb853ba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GoginMA.Sprint2.Task4.V20/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GoginMA.Sprint2.Task4.V20.Lib;
+
+namespace Tyuiu.GoginMA.Sprint2.Task4.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Гогин М. А. | АСОиУБ-23-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Тернарный оператор                                               *");
+            Console.WriteLine("* Задание #4                                                             *");
+            Console.WriteLine("* Вариант #20                                                            *");
+            Console.WriteLine("* Выполнил: Гогин Михаил Александрович | АСОиУБ-23-1                     *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение               *");
+            Console.WriteLine("* с использованием тернарного оператора, где пользователь вводит         *");
+            Console.WriteLine("* значение переменных x,y. Ответ округлить до 3 знаков после запятой.    *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("Введите значение переменной X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x, y);
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("Значение функции = " + res);
+            Console.ReadKey();
+        }
+    }
+}

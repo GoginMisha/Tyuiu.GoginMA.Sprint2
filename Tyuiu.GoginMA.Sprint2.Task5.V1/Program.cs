ccb88dd211191e16d4a283e3926ed9971daf1185
Tyuiu.GoginMA@outlook.com <partycat53241@gmail.com>
2023-10-15 17:03:14 +0500

    Task5

---
+++ Tyuiu.GoginMA.Sprint2.Task5.V1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GoginMA.Sprint2.Task5.V1.Lib;
+
+namespace Tyuiu.GoginMA.Sprint2.Task5.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Гогин М. А. | АСОиУБ-23-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Оператор switch                                                  *");
+            Console.WriteLine("* Задание #5                                                             *");
+            Console.WriteLine("* Вариант #1                                                             *");
+            Console.WriteLine("* Выполнил: Гогин Михаил Александрович | АСОиУБ-23-1                     *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет       *");
+            Console.WriteLine("* требуемое значение и возвращает результат. Условие:                    *");
+            Console.WriteLine("* По данному номеру месяца, определить количество дней в этом месяце.    *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("Введите номер месяца: ");
+            int numMonth = Convert.ToInt32(Console.ReadLine());
+            int res;
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            if ((numMonth < 1) || (numMonth > 12))
+            {
+                Console.WriteLine("Введено неверное значение!");
+            }
+            else
+            {
+                Console.WriteLine("Количество дней в этом месяце равно: " + ds.FindMonthDaysCount(numMonth));
+            }
+            Console.ReadKey();
+        }
+    }
+}
